1835b591ba9715f473288ada12c9ebe431a4c42f
Usmoxan <101461756+Usmoxan@users.noreply.github.com>
2023-04-13 17:32:40 +0500

    fixed

--- lib/color_shade_generator/shade_generator_main.dart
+++ lib/color_shade_generator/shade_generator_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gradient_maker/color_shade_generator/shade_color_picker.dart';
 import 'models/color_model.dart';
 
@@ -51,42 +52,53 @@ class _ColorShadesScreenState extends State<ColorShadesScreen> {
                 final shade = _colorModel.shades[index];
                 final hexColor =
                     '#${shade.value.toRadixString(16).substring(2)}';
-                return Container(
-                  color: shade,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        hexColor,
-                        style: const TextStyle(
-                          shadows: <Shadow>[
-                            Shadow(
-                              offset: Offset(3.0, 0.0),
-                              blurRadius: 3.0,
-                              color: Color.fromARGB(255, 0, 0, 0),
-                            ),
-                          ],
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
+                return GestureDetector(
+                  onTap: () {
+                    Clipboard.setData(ClipboardData(text: hexColor));
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        duration: Duration(milliseconds: 4000), // default 4s
+                        content: Text('Copied!'),
                       ),
-                      const SizedBox(height: 10),
-                      Text(
-                        '${shade.red}, ${shade.green}, ${shade.blue}',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          shadows: <Shadow>[
-                            Shadow(
-                              offset: Offset(3.0, 0),
-                              blurRadius: 3.0,
-                              color: Color.fromARGB(255, 0, 0, 0),
-                            ),
-                          ],
+                    );
+                  },
+                  child: Container(
+                    color: shade,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          hexColor,
+                          style: const TextStyle(
+                            shadows: <Shadow>[
+                              Shadow(
+                                offset: Offset(3.0, 0.0),
+                                blurRadius: 3.0,
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ],
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 10),
+                        Text(
+                          '${shade.red}, ${shade.green}, ${shade.blue}',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            shadows: <Shadow>[
+                              Shadow(
+                                offset: Offset(3.0, 0),
+                                blurRadius: 3.0,
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
